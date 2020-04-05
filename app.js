41ff672c0824d2ee74fbe2e531539e0cb73861c5
KavishkaRajakaruna <rmkhrajakaruna@std.appsc.sab.ac.lk>
2020-04-05 16:12:31 +0530

    Global objects'

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('Hey kavishka');
+
+setTimeout(function(){
+    console.log('3 sec. has passed');
+}, 3000);
+
+
+// var time = 0;
+// setInterval(function(){
+//     time += 2;
+//     console.log(time + 'seconds have passed');
+// },2000);
+
+console.log(__dirname);
+console.log(__filename);
