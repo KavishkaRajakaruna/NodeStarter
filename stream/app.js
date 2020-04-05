dc515771741e36f038008a2d1862dce359d37652
KavishkaRajakaruna <rmkhrajakaruna@std.appsc.sab.ac.lk>
2020-04-05 20:15:33 +0530

    Pipe tutorial

--- stream/app.js
+++ stream/app.js
@@ -4,8 +4,9 @@ var fs = require('fs');
 var myReadStream = fs.createReadStream(__dirname+ '/readme.txt','utf8');
 var myWriteStream = fs.createWriteStream(__dirname+'/writeMe.txt');
 
-myReadStream.on('data', function(chunk){
-    console.log('New Chunk recieved');
-    myWriteStream.write(chunk);
-});
-
+// myReadStream.on('data', function(chunk){
+//     console.log('New Chunk recieved');
+//     myWriteStream.write(chunk);
+// });
+//Replaced all the above from pipe
+myReadStream.pipe(myWriteStream);
